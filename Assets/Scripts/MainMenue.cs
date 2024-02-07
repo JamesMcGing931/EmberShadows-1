ee40643f0c03de2960dadf209f511f9822b41827
GarethBergin <garethbergin2003@gmail.com>
2024-02-07 22:37:11 +0000

    Update 07/02/2024

--- Assets/Scripts/MainMenue.cs
+++ Assets/Scripts/MainMenue.cs
@@ -5,8 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenue : MonoBehaviour
 {
+    public float time = 0.0f;
+
+    void Start()
+    {
+        time = 0.0f;
+    }
+    void Update()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                SceneManager.LoadSceneAsync(1);
+            }
+        }
+
+        Debug.Log("Time: " + time);
+    }
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        time += 5.0f;
+    }
+
+        public void ExitGame()
+    {
+        Application.Quit();
     }
 }
