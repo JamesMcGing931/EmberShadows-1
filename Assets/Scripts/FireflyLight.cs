2286dfeaf11f8b05bcaab532248ccfb248790b61
GarethBergin <garethbergin2003@gmail.com>
2024-02-19 00:03:59 +0000

    Level1 Update

---
+++ Assets/Scripts/FireflyLight.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
+
+public class FireflyLight : MonoBehaviour
+{
+    public Light2D spotlight;
+    public float minIntensity = 0.2f;
+    public float maxIntensity = 0.6f;
+    public float minOuterRadius = 1f;
+    public float maxOuterRadius = 1.5f;
+    public float minSpeed = 1f;
+    public float maxSpeed = 3f;
+
+    private float targetIntensity;
+    private float targetOuterRadius;
+    private float currentIntensity;
+    private float currentOuterRadius;
+    private float changeIntensitySpeed;
+    private float changeRadiusSpeed;
+
+    void Start()
+    {
+        InitializeTargetValues();
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(currentIntensity - targetIntensity) < 0.01f)
+            InitializeTargetValues();
+
+        currentIntensity = Mathf.MoveTowards(currentIntensity, targetIntensity, changeIntensitySpeed * Time.deltaTime);
+        currentOuterRadius = Mathf.MoveTowards(currentOuterRadius, targetOuterRadius, changeRadiusSpeed * Time.deltaTime);
+
+        spotlight.intensity = currentIntensity;
+        spotlight.pointLightOuterRadius = currentOuterRadius;
+    }
+
+    void InitializeTargetValues()
+    {
+        targetIntensity = Random.Range(minIntensity, maxIntensity);
+        targetOuterRadius = Random.Range(minOuterRadius, maxOuterRadius);
+        changeIntensitySpeed = Random.Range(minSpeed, maxSpeed);
+        changeRadiusSpeed = Random.Range(minSpeed, maxSpeed);
+    }
+}
---
+++ Assets/Scripts/collectibleitem.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
+
+public class CollectableItem : MonoBehaviour
+{
+    public ItemManager im;
+    public float spotlightRadiusIncrease = 1f;
+    public GameObject player;
+    public GameObject interactionPrompt; // Reference to the 2D GameObject representing the "E" key
+
+    private bool isCollected = false;
+    private bool inRange = false;
+    public int fireflyCount;
+    public Text fireflyText;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("player") && !isCollected)
+        {
+            inRange = true;
+            ShowInteractionPrompt(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("player"))
+        {
+            inRange = false;
+            ShowInteractionPrompt(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (isCollected)
+            return;
+
+        if (inRange && Input.GetKeyDown(KeyCode.E))
+        {
+            CollectItem();
+        }
+    }
+
+    void CollectItem()
+    {
+        isCollected = true;
+        IncreasePlayerSpotlightRadius();
+        Debug.Log("Item collected.");
+        Destroy(gameObject); // Destroy the collectable item after it's collected
+        im.flyCount++;
+        Debug.Log("Items: " + im.flyCount);
+        fireflyText.text = im.flyCount.ToString();
+    }
+
+    void IncreasePlayerSpotlightRadius()
+    {
+        SpotlightPulse playerSpotlight = player.GetComponentInChildren<SpotlightPulse>();
+        if (playerSpotlight != null)
+        {
+            // Increase min and max outer radius
+            playerSpotlight.minOuterRadius += spotlightRadiusIncrease;
+            playerSpotlight.maxOuterRadius += spotlightRadiusIncrease;
+
+            Debug.Log("Min Outer Radius: " + playerSpotlight.minOuterRadius);
+            Debug.Log("Max Outer Radius: " + playerSpotlight.maxOuterRadius);
+        }
+        else
+        {
+            Debug.LogWarning("Player's spotlight not found!");
+        }
+    }
+
+    void ShowInteractionPrompt(bool show)
+    {
+        interactionPrompt.SetActive(show);
+    }
+}
